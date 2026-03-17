3a65dc7f34659a45cf3f6918baaeddafc4c8ed6a
anadeheza <adehezaflaherty@gmail.com>
2026-03-17 18:17:36 -0300

    Add files via upload

--- cart.js
+++ cart.js
@@ -180,14 +180,12 @@ menu.classList.toggle("show")
 
 }
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function() {
+  const header = document.querySelector("header");
 
-let header = document.querySelector("header")
-
-if(window.scrollY > 50){
-header.classList.add("scrolled")
-}else{
-header.classList.remove("scrolled")
-}
-
-})
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
